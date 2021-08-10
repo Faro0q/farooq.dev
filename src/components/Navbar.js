ed313a50570c83356a974a6347fce799627401d0
Farooq Syed <syed@farooq.dev>
2021-08-10 17:33:54 -0500

    Added Nav Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,30 @@
+import {Link } from 'react-router-dom'
 import React from 'react'
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+    height: 60px;
+    background: red;
+`;
+
+const Logo = styled(Link)`
+    color: #fff;
+`;
+
+const MenuBars = styled.i``;
+
+const NavMenu = styled.div``;
 
 const Navbar = () => {
     return (
-        <div>
+        <Nav>
+            <Logo>ELIXYT</Logo>
+            <MenuBars />
+            <NavMenu>
+
+            </NavMenu>
             <h1>Navbar</h1>
-        </div>
+        </Nav>
     )
 }
 
