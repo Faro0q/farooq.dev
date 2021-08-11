e09d9cdb5c8b818cee256d5b5c3d4d9864c26024
Farooq Syed <syed@farooq.dev>
2021-08-10 21:54:14 -0500

    Added a fully working Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,64 @@
 import {Link } from 'react-router-dom'
 import React from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components/macro'
+import {menuData} from '../data/MenuData'
+import {FaBars} from 'react-icons/fa'
 
 const Nav = styled.nav`
-    height: 60px;
-    background: red;
+    max-width: 430px;
+    width: 100%;
+    margin: auto;
+    background-color: #010409;
+    text-align: center;
+    border-radius: 100px;
+    padding-bottom: 19px;
 `;
 
-const Logo = styled(Link)`
-    color: #fff;
+const NavLink = css`
+    text-decoration: none;
+    padding: 0 25px;
+    color: #f1f6ff;
+    font-size: 30px;
+    font-weight: 700;
+    display: inline-block
 `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled(FaBars)`
+    padding: 1rem 2rem;
+    display: none;
+    
+    @media screen and (max-width: 250px){
+        display: block;
+        cursor: pointer;
+        position: absolute;
+        transform: translate(-20%, -35%);
+    }
+`;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+    @media screen and (max-width: 250px){
+        display: none;
+    }
+`;
+
+const NavMenuLinks = styled(Link)`
+    ${NavLink}
+    
+`;
+
+
 
 const Navbar = () => {
     return (
         <Nav>
-            <Logo>ELIXYT</Logo>
             <MenuBars />
             <NavMenu>
-
+                {menuData.map((item, index) => (
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
             </NavMenu>
-            <h1>Navbar</h1>
         </Nav>
     )
 }
