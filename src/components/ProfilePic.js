69516e991df2206aebd79f6c51027f78d3757b76
Farooq Syed <syed@farooq.dev>
2021-08-10 23:20:00 -0500

    Updated with profile picture

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import styled, {css} from 'styled-components/macro'
+import styled from 'styled-components/macro'
+import ReactRoundedImage from "react-rounded-image";
+import pic from '../images/me-pic.png'
 
 const ProfileSection = styled.section`
     height: 100vh;
     max-height: 1100px;
     position:relative;
     overflow: hidden;
+    borderRadius: 40;
 `;
 
 const ProfileWrapper = styled.div`
     width: 100%;
-    height: 50%;
+    height: 70%;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -22,7 +25,15 @@ const ProfilePic = () => {
     return (
         <ProfileSection>
             <ProfileWrapper>
-                <h1>Heroo</h1>
+            <ReactRoundedImage
+                image={pic}
+                roundedColor="green"
+                imageWidth="400"
+                imageHeight="500"
+                roundedSize="10"
+                borderRadius="500"
+                hoverColor="#DD1144"
+                />
             </ProfileWrapper>
         </ProfileSection>
     )
