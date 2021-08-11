03eebf86346d3fb0f75a989ba8b3ee65d206f541
Farooq Syed <syed@farooq.dev>
2021-08-10 21:54:51 -0500

    Created a global style class

---
+++ src/globalStyles.js
@@ -0,0 +1,17 @@
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        // margin: 0;
+        // padding: 0;
+        // box-sizing: border-box;
+        padding-top: 5px;
+        font-family; 'Montserrat', sans-serif;
+    }
+
+    html, body {
+        overflow-x: hidden;
+    }
+`;
+
+export default GlobalStyle;
