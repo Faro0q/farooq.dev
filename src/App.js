68ce468d9b095893904ec276bf4b7f5951f408a1
Farooq Syed <syed@farooq.dev>
2021-08-10 21:54:40 -0500

    Added globalstyle nav and pic to the main app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import ProfilePic from './components/ProfilePic';
+import GlobalStyle from './globalStyles';
 
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Navbar />
+      <ProfilePic />
     </>
   );
 }
