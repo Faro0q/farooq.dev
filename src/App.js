334a6b8bc10d5f3a57a2a425e333ad09efe7a465
Farooq Syed <syed@farooq.dev>
2021-08-12 04:45:46 -0500

    Added Resume page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Courses from './pages/Courses';
+import Resume from './pages/Resume';
 import Error from './pages/Error';
 import Navigation from './components/Navbar'
 import SocialFollow from './components/SocialFollow';
+import GlobalStyle from './globalStyles.js';
+
  
 class App extends Component {
   render() {
@@ -18,9 +21,11 @@ class App extends Component {
              <Route path="/" component={Home} exact/>
              <Route path="/about" component={About} />
              <Route path="/courses" component={Courses}/>
+             <Route path="/resume" component={Resume}/>
              <Route component={Error}/>
            </Switch>
            <SocialFollow />
+           <GlobalStyle/>
         </div> 
       </BrowserRouter>
     );
---
+++ src/pages/Resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import image1 from '../images/Resume2.png';
+import '../App.css'
+
+function Resume() {
+
+    return (
+        <div class="resume">
+            <center>
+        <img src={image1} alt="image not found" ></img></center>
+        </div>
+    );
+}
+
+export default Resume
